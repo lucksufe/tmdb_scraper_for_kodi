65e13d71ff2f7d327c4abd41891cca93b43225e5
unknown <459397707@qq.com>
2023-06-19 21:01:04 +0800

    fix issue

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def deal_with_tv(tv_dir_path, search_key, language):
     best_match = info["results"][0]
     download(url=f"{base_url}/{poster_sizes[-1]}{best_match['poster_path']}", save_dir=tv_dir_path)
     download(url=f"{base_url}/{backdrop_sizes[-1]}{best_match['backdrop_path']}", save_dir=tv_dir_path)
-    generate_nfo(f_name=os.path.join(tv_dir_path, 'tvshow.nfo'), nfo_type=NFOType.movie, detail=best_match)
+    generate_nfo(f_name=os.path.join(tv_dir_path, 'tvshow.nfo'), nfo_type=NFOType.tv, detail=best_match)
     for root, dirs, files in os.walk(tv_dir_path):
         for file_name in files:
             if any([file_name.endswith(media_suffix) for media_suffix in [".mkv", ".mp4"]]):
@@ -32,7 +32,7 @@ def deal_with_episode(episode_file, info):
 
 if __name__ == "__main__":
     # deal_with_movie(movie_file=r"E:\aliyun\新建文件夹\test\[BeanSub&FZSD][Jigokuraku][11][GB][1080P][x264_AAC].mp4", search_key="阿甘正传", language=Language.zh)
-    deal_with_tv(tv_dir_path=r"E:\aliyun\新建文件夹", search_key="地狱乐", language=Language.zh)
+    deal_with_tv(tv_dir_path=r"\\192.168.31.183\公共空间\动漫\一拳超人", search_key="一拳超人", language=Language.zh)
     # import sys
     # from optparse import OptionParser
 
